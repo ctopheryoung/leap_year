95a35b3fa615e7687efcca2cf2b7db2301863905
Chris Young and Trevor Elvey <ctopheryoung@gmail.com>
2016-01-19 09:21:56 -0800

    refactor leapYear

--- spec/specs.js
+++ spec/specs.js
@@ -7,5 +7,7 @@ describe('leapYear', function() {
     expect(leapYear(2004)).to.equal(true);
   });
 
-
+  it ("is false for years divisible by 100", function() {
+    expect(leapYear(1900)).to.equal(false);
+  });
 });
